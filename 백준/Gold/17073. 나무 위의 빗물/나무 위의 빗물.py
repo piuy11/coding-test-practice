e96a1b34bb25b6f00d554f63ad492cf112a79f02
piuy11 <34086332+piuy11@users.noreply.github.com>
2022-10-20 04:36:10 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 1092 ms, Memory: 269724 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -6,7 +6,6 @@ input = sys.stdin.readline
 n, w = map(int, input().split())
 adjacent_list = defaultdict(list)
 checked = defaultdict(bool)
-divider = defaultdict(int)
 for _ in range(n-1):
     u, v = map(int, input().split())
     adjacent_list[u].append(v)
@@ -14,9 +13,7 @@ for _ in range(n-1):
 
 # find leaf nodes of tree
 queue = deque([1])
-divider[1] = 1
 leaves_count = 0
-leaves_sum = 0
 if n == 1 or n == 2:
     print(w)
 else:
@@ -24,18 +21,11 @@ else:
         node = queue.pop()
         checked[node] = True
 
-        # is leaf node
         if len(adjacent_list[node]) == 1 and node != 1:
             leaves_count += 1
-            leaves_sum += 1 / divider[node]
 
-        if node == 1:
-            div = len(adjacent_list[node])
-        else:
-            div = len(adjacent_list[node]) - 1
         for i in adjacent_list[node]:
             if not checked[i]:
                 queue.append(i)
-                divider[i] = divider[node] * div
 
     print(w / leaves_count)
